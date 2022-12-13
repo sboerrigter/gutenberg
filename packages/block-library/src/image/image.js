2eec4ac5ed63f40c9976faf631eab7bde568ed9b
Daniel Richards <daniel.richards@automattic.com>
2022-12-13 16:21:01 +0800

    Try allowing resize handles on wide and full aligned images

--- packages/block-library/src/image/image.js
+++ packages/block-library/src/image/image.js
@@ -161,10 +161,7 @@ export default function Image( {
 	const [ isEditingImage, setIsEditingImage ] = useState( false );
 	const [ externalBlob, setExternalBlob ] = useState();
 	const clientWidth = useClientWidth( containerRef, [ align ] );
-	const isResizable =
-		allowResize &&
-		! isContentLocked &&
-		! ( isWideAligned && isLargeViewport );
+	const isResizable = isLargeViewport && allowResize && ! isContentLocked;
 	const imageSizeOptions = map(
 		imageSizes.filter( ( { slug } ) =>
 			get( image, [ 'media_details', 'sizes', slug, 'source_url' ] )
@@ -505,15 +502,9 @@ export default function Image( {
 	);
 
 	let imageWidthWithinContainer;
-	let imageHeightWithinContainer;
-
 	if ( clientWidth && naturalWidth && naturalHeight ) {
 		const exceedMaxWidth = naturalWidth > clientWidth;
-		const ratio = naturalHeight / naturalWidth;
 		imageWidthWithinContainer = exceedMaxWidth ? clientWidth : naturalWidth;
-		imageHeightWithinContainer = exceedMaxWidth
-			? clientWidth * ratio
-			: naturalHeight;
 	}
 
 	if ( canEditImage && isEditingImage ) {
@@ -531,9 +522,6 @@ export default function Image( {
 	} else if ( ! isResizable || ! imageWidthWithinContainer ) {
 		img = <div style={ { width, height } }>{ img }</div>;
 	} else {
-		const currentWidth = width || imageWidthWithinContainer;
-		const currentHeight = height || imageHeightWithinContainer;
-
 		const ratio = naturalWidth / naturalHeight;
 		const minWidth =
 			naturalWidth < naturalHeight ? MIN_SIZE : MIN_SIZE * ratio;
@@ -568,6 +556,10 @@ export default function Image( {
 			/>
 		);
 
+		const resizableContainerWidth = isWideAligned
+			? '100%'
+			: width ?? 'auto';
+
 		img = (
 			<ResizableAlignmentControls
 				allowedAlignments={ [ 'none', 'wide', 'full' ] }
@@ -578,11 +570,12 @@ export default function Image( {
 				minHeight={ minHeight }
 				maxHeight={ maxWidthBuffer / ratio }
 				onResizeStart={ onResizeStart }
-				onResizeStop={ ( event, direction, elt, delta ) => {
+				onResizeStop={ ( newWidth, newHeight ) => {
 					onResizeStop();
 					setAttributes( {
-						width: parseInt( currentWidth + delta.width, 10 ),
-						height: parseInt( currentHeight + delta.height, 10 ),
+						align: 'none',
+						width: parseInt( newWidth, 10 ),
+						height: parseInt( newHeight, 10 ),
 					} );
 				} }
 				onSnap={ ( newAlignment ) => {
@@ -593,7 +586,7 @@ export default function Image( {
 				} }
 				showHandle={ isSelected }
 				size={ {
-					width: width ?? 'auto',
+					width: resizableContainerWidth,
 					height: height && ! hasCustomBorder ? height : 'auto',
 				} }
 			>
