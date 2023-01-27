b2d58f72aa56412d626bd8750f92124b25bd0039
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-01-27 14:49:44 +1000

    Add filter ensuring selectors metadata is available

--- lib/compat/wordpress-6.2/blocks.php
+++ lib/compat/wordpress-6.2/blocks.php
@@ -26,3 +26,25 @@ function gutenberg_safe_style_attrs_6_2( $attrs ) {
 	return $attrs;
 }
 add_filter( 'safe_style_css', 'gutenberg_safe_style_attrs_6_2' );
+
+/**
+ * Ensure the selectors property, set via block.json metadata, is included
+ * within the block type's settings.
+ *
+ * Note: This should be removed when the minimum required WP version is >= 6.2.
+ *
+ * @see https://github.com/WordPress/gutenberg/pull/46496
+ *
+ * @param array $settings Current block type settings.
+ * @param array $metadata Block metadata as read in via block.json.
+ *
+ * @return array Filtered block type settings.
+ */
+function gutenberg_add_selectors_to_block_type_settings( $settings, $metadata ) {
+	if ( ! isset( $settings['selectors'] ) && isset( $metadata['selectors'] ) ) {
+		$settings['selectors'] = $metadata['selectors'];
+	}
+
+	return $settings;
+}
+add_filter( 'block_type_metadata_settings', 'gutenberg_add_selectors_to_block_type_settings', 10, 2 );
